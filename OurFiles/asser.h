315af86c4f3d7e3ddb1d9e4a70a75e734e5447e2
Nicolas Gandolfo <n.gandolfo@gmail.com>
2021-07-07 12:55:23 +0200

    Odométrie OK

--- OurFiles/asser.h
+++ OurFiles/asser.h
@@ -1,15 +1,15 @@
 
 #define N				2						// Nombre de moteurs
-#define DEFAULT_KP		30						// Coefficient proporionnel 
+#define DEFAULT_KP		20						// Coefficient proporionnel 
 #define DEFAULT_KI		0						// Coefficient integral (inverse !)
 #define DEFAULT_KD		200						// Coefficient derive
 #define CODEUR			1024 					// Nombre de pas par tour moteur (sans le ratio x4)
 #define REDUCTEUR		1						// Reducteur utilise en sortie d'arbre moteur (=1 si roue codeuse indépendante)
 #define DIAMETRE_ROUE 	58.646						// Diametre de la roue motrice (ou roue codeuse si indépendante) en mm 
 #define PI 				3.1415926535897932384	// Ben pi quoi
-#define VOIE			224 // Distance entre les deux roues en mm
+#define VOIE			223.1347 // Distance entre les deux roues en mm
 #define COEFF_ROUE		1			// Coeff d'ajustement pour le diametre de la roue
-#define COEFF_ROUE_DIFF 1
+#define COEFF_ROUE_DIFF 1.00065
 #define MM_SCALER		COEFF_ROUE*DIAMETRE_ROUE*PI/(4*CODEUR*REDUCTEUR) // Formule de conversion [pas]<==>[mm]
 #define MM_INVSCALER	4*CODEUR*REDUCTEUR/(COEFF_ROUE*DIAMETRE_ROUE*PI)
 #define DEFAULT_SPEED	500						// Vitesse par défaut en mm/s
